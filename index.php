efbff9acac51f6b80a826ad738bf5a684d8a1cfc
Gerwin15 <gerwin_cando@rcc.edu.ph>
2024-04-20 23:14:25 +0800

    add home page

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unit Converter</title>
+
+</head>
+
+    <style>
+        body {background-color: #F7DCB9}
+        h1 {text-align: center;}
+        h1 {font-family: Verdana}
+        p {text-align: center;}
+        p {font-family: Verdana}
+        li {text-align: center;}
+        li {font-family: Verdana}
+    </style>
+
+    <body>
+     
+    <h1>Welcome to Gerwin's Unit Converter!</h1>
+    <p>Please select an option:</p>
+
+        <li><a href="weight.php">Weight Converter</a></li>
+        <li><a href="height.php">Height Converter</a></li>
+    
+    </body>
+</html>
